367d40eda0cd79b3dbb4f4c2a97e9df948d75a64
023arpita <arpita0267.becse24@chitkara.edu.in>
2025-03-27 21:25:28 +0530

    Solution to Find the K-th Smallest Element

--- Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
-int main(){
-    int n,k;
-    scanf("%d", &n);
-    int arr[n];
+#include <limits.h> // For INT_MAX
 
-    for(int i = 0; i < n; i++){
-        scanf("%d", &arr[i]);
+// Function to find the k-th smallest element
+int kthSmallest(int arr[], int n, int k) {
+    for (int i = 0; i < k; i++) {  
+        int minIndex = -1;
+        int minValue = INT_MAX;
 
-    scanf("%d", &k);
-
-
-
-
-
-    int result = kthsmallest(arr , n , k );
-    printf("%d", result);
-    return 0;
-
-
-
-
+        // Find the smallest element in the array that hasn't been selected yet
+        for (int j = 0; j < n; j++) {
+            if (arr[j] < minValue) {
+                minValue = arr[j];
+                minIndex = j;
+            }
+        }
+        
+        // Mark this element as used by setting it to a very large number
+        arr[minIndex] = INT_MAX;
+        
+        // If this is the k-th smallest element, return it
+        if (i == k - 1)
+            return minValue;
     }
+    return -1; // Should never happen if input is valid
+}
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+    
+    int arr[n];  
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);  
+
+    if (k > 0 && k <= n) {
+        int result = kthSmallest(arr, n, k);
+        printf("%d\n", result);
+    } else {
+        printf("Invalid k value\n");
+    }
+    
+    return 0;
 }
